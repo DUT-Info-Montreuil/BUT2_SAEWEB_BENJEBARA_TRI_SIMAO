8d2d2d83a4605c32f1495e45624f1e4f2517e3cb
Chikage04 <104400056+Chikage04@users.noreply.github.com>
2025-01-21 02:18:35 +0100

    correction ajout note + bouton d'hash des mdp

---
+++ cont_connexion.php
@@ -0,0 +1,44 @@
+<?php
+require_once 'modele_connexion.php';
+require_once 'vue_connexion.php';
+
+class ContConnexion {
+    private $modele;
+    private $vue;
+
+    public function __construct() {
+        $this->modele = new ModeleConnexion();
+        $this->vue = new VueConnexion();
+        session_start();
+    }
+
+    public function form() {
+        if (isset($_SESSION['id_utilisateur'])) {
+            header("Location: index.php?module=projets");
+            exit();
+        }
+        $this->vue->form_inscription();
+    }
+
+    public function ajout_data() {
+        if (isset($_POST['email'], $_POST['passwd'])) {
+            $email = htmlspecialchars($_POST['email']);
+            $passwd = $_POST['passwd'];
+            
+            $user_id = $this->modele->verifierConnexion($email, $passwd);
+            
+            if ($user_id) {
+                $_SESSION['id_utilisateur'] = $user_id;
+                $_SESSION['email'] = $email;
+                header("Location: index.php?module=projets");
+                exit();
+            } else {
+                $this->vue->afficherErreur("Email ou mot de passe incorrect");
+            }
+        }
+    }
+    public function hashAllPasswords() {
+        $this->modele->hashAllPasswords();
+    }
+}
+?>
---
+++ vue_connexion.php
@@ -0,0 +1,56 @@
+<?php
+class VueConnexion {
+    public function form_inscription() {
+        ?>
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Connexion</title>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma/css/bulma.min.css">
+        </head>
+        <body>
+        <a href="index.php?module=deconnexion">Déconnexion</a>
+            <section class="section">
+                <div class="container">
+                    <div class="columns is-centered">
+                        <div class="column is-half">
+                            <form action="index.php?module=connexion&action=ajout" method="post" class="box">
+                                <div class="field">
+                                    <label class="label">Email</label>
+                                    <div class="control">
+                                        <input class="input" type="text" name="email" required>
+                                    </div>
+                                </div>
+                                <div class="field">
+                                    <label class="label">Mot de passe</label>
+                                    <div class="control">
+                                        <input class="input" type="password" name="passwd" required>
+                                    </div>
+                                </div>
+                                <div class="field">
+                                    <div class="control">
+                                        <button class="button is-primary" type="submit">Se connecter</button>
+                                    </div>
+                                </div>
+                            </form>
+                            <form action="index.php?module=connexion&action=hash_passwords" method="post" class="box">
+                                <div class="field">
+                                    <div class="control">
+                                        <button class="button is-warning" type="submit">Hash All Passwords</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </body>
+        </html>
+        <?php
+    }
+
+    public function afficherErreur($message) {
+        echo '<div class="notification is-danger">' . htmlspecialchars($message) . '</div>';
+    }
+}
+?>
